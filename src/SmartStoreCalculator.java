3556aa42ad2753987ffd3c3a7a5811ebb6abb306
ezequias-dev <ezequiasd30@gmail.com>
2026-02-23 18:47:16 -0300

    smart store calculator

---
+++ src/SmartStoreCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SmartStoreCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("==== SMART STORE CALCULATOR ====");
+
+        System.out.print("Product name: ");
+        String productName = input.nextLine();
+
+        System.out.print("Unit price: ");
+        double unitPrice = input.nextDouble();
+    }
+}
